cb08d65fc1ccd6fed1d983fd8efc7bf3e4d6fad3
dhanush2231 <124229674+dhanush2231@users.noreply.github.com>
2023-02-02 09:51:44 +0530

    sagtg
    adgdg

---
+++ node-intro/index.js
@@ -0,0 +1,8 @@
+const supHeroes = require("superheroes");
+const supVillain= require("supervillains");
+
+var supHeroName = supHeroes.random();
+var supVillName = supVillain.random();
+
+console.log(supHeroName);
+console.log(supVillName);
